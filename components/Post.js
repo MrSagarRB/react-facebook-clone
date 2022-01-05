558e82bac686ccdda1359645cab3e6ef5e049f92
Sagar Borude <sagar.html5@gmail.com>
2022-01-05 22:23:16 +0530

    feed section updated

--- components/Post.js
+++ components/Post.js
@@ -1,12 +1,61 @@
-function Post({ name, message, email, postImage, image }) {
+import { ChatAltIcon, ShareIcon, ThumbUpIcon } from "@heroicons/react/solid";
+import Image from "next/image";
+import React from "react";
+
+function Post({ name, massage, email, postImage, image, timestamp }) {
   return (
     <div className="flex flex-col">
-      <div>
-        <div>
-          <img className="rounded-full" src={image} width={40} height={40} />
+      <div className="p-5 bg-white mt-5 rounded-t-2xl shadow-sm">
+        <div className="flex items-center space-x-2">
+          <img
+            src={image}
+            width={40}
+            height={40}
+            className="rounded-full"
+            alt="Profile"
+          />
+          <div>
+            <p className="font-medium">{name}</p>
+
+
+              {timestamp ? (
+              <p className="text-xs text-gray-400">
+              {new Date(timestamp?.toDate()).toLocaleString()}
+              </p>
+              ):(
+                <p className="text-xs text-gray-400">{name}</p>
+              )}
+
+          </div>
         </div>
-        <div>
-            <p>{name}</p>
+        <p className="pt-4 text-gray-800">{massage}</p>
+      </div>
+      {postImage && (
+        <div className="relative h-56 md:h-96 bg-white">
+          <Image
+            src={postImage}
+            objectFit="cover"
+            layout="fill"
+            placeholder="Wait Image Loading..."
+            alt="PostImage"
+            priority
+          />
+        </div>
+      )}
+
+      {/* footer */}
+      <div className="flex justify-between items-center rounded-b-2xl bg-white shadow-md text-gray-400 border-t">
+        <div className="inputIcon rounded-none rounded-bl-2xl lcs">
+          <ThumbUpIcon className="h-4" />
+          <p className="text-sm sm:text-base">Like</p>
+        </div>
+        <div className="inputIcon rounded-none lcs">
+          <ChatAltIcon className="h-4" />
+          <p className="text-sm sm:text-base">Comment</p>
+        </div>
+        <div className="inputIcon rounded-none rounded-br-2xl lcs">
+          <ShareIcon className="h-4" />
+          <p className="text-sm sm:text-base">Share</p>
         </div>
       </div>
     </div>
--- components/Posts.js
+++ components/Posts.js
@@ -1,33 +1,41 @@
-import {useCollection} from "react-firebase-hooks/firestore";
-import {db} from "../firebase";
+import React from "react";
+import Post from "./Post";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
 
 
-function Posts() {
-  const [realtimePosts] = useCollection(db.collection("posts"));
+function Posts({ posts }) {
+    const [realtimePosts] = useCollection(
+      db.collection("posts").orderBy("timestamp", "desc")
+    );
 
-  return <div>
-{realtimePosts?.docs.map((post)=>(
-    <Post 
-       key={post.id}
-        name={post.data().name}
-        message={post.data().message}
-        email={post.data().email}
-        image={post.data().image}
-        postImage={post.data().postImage}
+  return (
+    <div>
+      {realtimePosts ? 
+      realtimePosts?.docs.map((post) => (
+        <Post
+          key={post.id}
+          name={post.data().name}
+          massage={post.data().massage}
+          email={post.data().email}
+          timestamp={post.data().timestamp}
+          image={post.data().image}
+          postImage={post.data().postImage}
+        />
+        )) 
+        : posts.map((post) => (
+            <Post
+            key={post.id}
+            name={post.name}
+            massage={post.massage}
+            email={post.email}
+            timestamp={post.timestamp}
+            image={post.image}
+            postImage={post.postImage}
+          />
+          )
+          )}
 
-        
-
-
-       />
-))}
-
-
-
-
-
-
-
-  </div>;
+    </div>
+  );
 }
-
-export default Posts;
